f919ab205519a6c6ecaafa74540ac332f349771f
davidevaliante <dav.valiante@gmail.com>
2022-03-07 11:15:31 +0100

    cache system

--- src/api/dreamcatcher.ts
+++ src/api/dreamcatcher.ts
@@ -1,104 +1,105 @@
-import express, { Response, Request } from "express";
-import { SpinModel } from "../mongoose-models/dreamcatcher/Spin";
-import { LightningDiceSpin } from "../mongoose-models/dreamcatcher/Spin";
-import {
-  getInitialPageData,
-  getLatestSpins,
-  getStatsInTheLastHours,
-} from "./get";
-const router = express.Router();
+import express, { Response, Request } from 'express'
+import { SpinModel } from '../mongoose-models/dreamcatcher/Spin'
+import { LightningDiceSpin } from '../mongoose-models/dreamcatcher/Spin'
+import { getInitialPageData, getLatestSpins, getStatsInTheLastHours } from './get'
+import cache from 'memory-cache'
 
-router.post("/write-spins", async (request: Request, response: Response) => {
-  console.log("writing");
-  try {
-    const list = request.body;
-    const bulkUpdate = await SpinModel.bulkWrite(
-      list.map((spin: LightningDiceSpin) => ({
-        updateOne: {
-          filter: { _id: spin._id },
-          update: { ...spin },
-          upsert: true,
-        },
-      }))
-    );
+const router = express.Router()
 
-    response.send({
-      updated: bulkUpdate.modifiedCount,
-      upserted: bulkUpdate.upsertedCount,
-    });
-  } catch (e) {
-    response.send({ error: e });
-  }
-});
-
-router.get("/get-all", async (request: Request, response: Response) => {
-  const allSpins = await SpinModel.find();
-  response.send({ allSpins });
-});
-
-router.get(
-  "/get-latest/:count",
-  async (request: Request, response: Response) => {
+router.post('/write-spins', async (request: Request, response: Response) => {
+    console.log('writing')
     try {
-      const { count } = request.params;
-      const latestSpins = await getLatestSpins(parseInt(count));
-      response.send({
-        latestSpins,
-      });
-    } catch (error) {
-      response.send({ error });
+        const list = request.body
+        const bulkUpdate = await SpinModel.bulkWrite(
+            list.map((spin: LightningDiceSpin) => ({
+                updateOne: {
+                    filter: { _id: spin._id },
+                    update: { ...spin },
+                    upsert: true,
+                },
+            }))
+        )
+
+        response.send({
+            updated: bulkUpdate.modifiedCount,
+            upserted: bulkUpdate.upsertedCount,
+        })
+    } catch (e) {
+        response.send({ error: e })
     }
-  }
-);
+})
 
-router.get("/get-hour", async (request: Request, response: Response) => {
-  try {
-    const now = new Date().getTime();
-    const timeSince = now - 1 * 60 * 60 * 1000 - 5 * 1000;
-    const spinsInTimeFrame = (await SpinModel.where("timeOfSpin")
-      .gte(timeSince)
-      .sort({ timeOfSpin: -1 })) as LightningDiceSpin[];
-    response.send({ spinsInTimeFrame });
-  } catch (error) {
-    response.send({ error });
-  }
-});
+router.get('/get-all', async (request: Request, response: Response) => {
+    const allSpins = await SpinModel.find()
+    response.send({ allSpins })
+})
 
-router.get(
-  "/stats-in-the-last-hours/:hours",
-  async (request: Request, response: Response) => {
+router.get('/get-latest/:count', async (request: Request, response: Response) => {
     try {
-      const { hours } = request.params;
-      const stats = await getStatsInTheLastHours(parseInt(hours));
-      response.send({ stats });
+        const { count } = request.params
+        const latestSpins = await getLatestSpins(parseInt(count))
+        response.send({
+            latestSpins,
+        })
     } catch (error) {
-      response.send({ error });
+        response.send({ error })
     }
-  }
-);
+})
 
-router.get("/delete-all", async (request: Request, response: Response) => {
-  const deleted = await SpinModel.deleteMany({});
-  const remaining = await SpinModel.find();
-  response.send({ deleted, remaining });
-});
-
-router.get(
-  "/data-for-the-last-hours/:hours",
-  async (request: Request, response: Response) => {
+router.get('/get-hour', async (request: Request, response: Response) => {
     try {
-      const { hours } = request.params;
-      const { spinsInTimeFrame, stats } = await getInitialPageData(
-        parseInt(hours)
-      );
-      response.send({
-        stats,
-        spinsInTimeFrame,
-      });
+        const now = new Date().getTime()
+        const timeSince = now - 1 * 60 * 60 * 1000 - 5 * 1000
+        const spinsInTimeFrame = (await SpinModel.where('timeOfSpin')
+            .gte(timeSince)
+            .sort({ timeOfSpin: -1 })) as LightningDiceSpin[]
+        response.send({ spinsInTimeFrame })
     } catch (error) {
-      response.send({ error });
+        response.send({ error })
     }
-  }
-);
+})
 
-export default router;
+router.get('/stats-in-the-last-hours/:hours', async (request: Request, response: Response) => {
+    try {
+        const { hours } = request.params
+        const stats = await getStatsInTheLastHours(parseInt(hours))
+        response.send({ stats })
+    } catch (error) {
+        response.send({ error })
+    }
+})
+
+router.get('/delete-all', async (request: Request, response: Response) => {
+    const deleted = await SpinModel.deleteMany({})
+    const remaining = await SpinModel.find()
+    response.send({ deleted, remaining })
+})
+
+router.get('/data-for-the-last-hours/:hours', async (request: Request, response: Response) => {
+    try {
+        const { hours } = request.params
+
+        if (parseInt(hours) == 24) {
+            console.log('response from cache')
+
+            const { stats, spinsInTimeFrame } = cache.get('24-hours-cache')
+
+            response.send({
+                stats,
+                spinsInTimeFrame,
+            })
+        } else {
+            const { spinsInTimeFrame, stats } = await getInitialPageData(parseInt(hours))
+            console.log('response NOT from cache')
+
+            response.send({
+                stats,
+                spinsInTimeFrame,
+            })
+        }
+    } catch (error) {
+        response.send({ error })
+    }
+})
+
+export default router
--- src/api/get.ts
+++ src/api/get.ts
@@ -1,82 +1,70 @@
-import {
-  LightningDiceSpin,
-  SpinModel,
-} from "../mongoose-models/dreamcatcher/Spin";
-import {
-  LightningDiceStats,
-  SymbolStats,
-} from "../mongoose-models/dreamcatcher/Stats";
-import { LightningSymbol } from "../mongoose-models/dreamcatcher/Symbols";
-import { MonopolyTableModel } from "../mongoose-models/Tables";
+import { LightningDiceSpin, SpinModel } from '../mongoose-models/dreamcatcher/Spin'
+import { LightningDiceStats, SymbolStats } from '../mongoose-models/dreamcatcher/Stats'
+import { LightningSymbol } from '../mongoose-models/dreamcatcher/Symbols'
+import { MonopolyTableModel } from '../mongoose-models/Tables'
+import cache from 'memory-cache'
 
 export const getLatestSpins = async (count: number) => {
-  const allSpins = await SpinModel.find().limit(count).sort({ timeOfSpin: -1 });
-  return allSpins;
-};
+    const allSpins = await SpinModel.find().limit(count).sort({ timeOfSpin: -1 })
+    return allSpins
+}
 
 export const getLatestTable = async () => {
-  const table = await MonopolyTableModel.find().limit(1).sort({ time: -1 });
-  return table;
-};
+    const table = await MonopolyTableModel.find().limit(1).sort({ time: -1 })
+    return table
+}
 
 export const getStatsInTheLastHours = async (hoursToCheck: number) => {
-  const now = new Date().getTime();
+    const now = new Date().getTime()
 
-  const timeSince = now - hoursToCheck * 60 * 60 * 1000 - 5 * 1000;
+    // console.log(cache.get('24-hours-cache'))
 
-  const spinsInTimeFrame = (await SpinModel.find(
-    { timeOfSpin: { $gte: timeSince } },
-    ["total", "multiplier"]
-  ).sort({ timeOfSpin: -1 })) as LightningDiceSpin[];
+    const timeSince = now - hoursToCheck * 60 * 60 * 1000 - 5 * 1000
 
-  const totalSpins = spinsInTimeFrame.length;
+    const spinsInTimeFrame = (await SpinModel.find({ timeOfSpin: { $gte: timeSince } }, ['total', 'multiplier']).sort({
+        timeOfSpin: -1,
+    })) as LightningDiceSpin[]
 
-  const stats = new LightningDiceStats(
-    timeSince,
-    totalSpins,
-    [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18].map(
-      (symbol: number) => {
-        const timeSince = spinsInTimeFrame.map((s) => s.total).indexOf(symbol);
+    const totalSpins = spinsInTimeFrame.length
 
-        const totalSymbolSpins = spinsInTimeFrame.filter(
-          (spin) => spin.total == symbol
-        );
+    const stats = new LightningDiceStats(
+        timeSince,
+        totalSpins,
+        [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18].map((symbol: number) => {
+            const timeSince = spinsInTimeFrame.map((s) => s.total).indexOf(symbol)
 
-        const totalSymbolMultiplier = totalSymbolSpins.reduce(
-          (p, c) => p + parseInt(c.multiplier.split("X")[0]),
-          0
-        );
+            const totalSymbolSpins = spinsInTimeFrame.filter((spin) => spin.total == symbol)
 
-        const avg = Math.ceil(totalSymbolMultiplier / totalSymbolSpins.length);
+            const totalSymbolMultiplier = totalSymbolSpins.reduce((p, c) => p + parseInt(c.multiplier.split('X')[0]), 0)
 
-        return new SymbolStats(
-          symbol,
-          (spinsInTimeFrame.filter((it) => it.total === symbol).length * 100) /
-            totalSpins,
-          timeSince != -1 ? timeSince : totalSpins,
-          spinsInTimeFrame.filter((it) => it.total === symbol).length,
-          avg
-        );
-      }
+            const avg = Math.ceil(totalSymbolMultiplier / totalSymbolSpins.length)
+
+            return new SymbolStats(
+                symbol,
+                (spinsInTimeFrame.filter((it) => it.total === symbol).length * 100) / totalSpins,
+                timeSince != -1 ? timeSince : totalSpins,
+                spinsInTimeFrame.filter((it) => it.total === symbol).length,
+                avg
+            )
+        })
     )
-  );
 
-  return stats;
-};
+    return stats
+}
 
 export const getInitialPageData = async (hoursToCheck: number) => {
-  const now = new Date().getTime();
+    const now = new Date().getTime()
 
-  const timeSince = now - hoursToCheck * 60 * 60 * 1000 - 5 * 1000;
+    const timeSince = now - hoursToCheck * 60 * 60 * 1000 - 5 * 1000
 
-  const spinsInTimeFrame = (await SpinModel.find({
-    timeOfSpin: { $gte: timeSince },
-  }).sort({ timeOfSpin: -1 })) as LightningDiceSpin[];
+    const spinsInTimeFrame = (await SpinModel.find({
+        timeOfSpin: { $gte: timeSince },
+    }).sort({ timeOfSpin: -1 })) as LightningDiceSpin[]
 
-  const stats = await getStatsInTheLastHours(hoursToCheck);
+    const stats = await getStatsInTheLastHours(hoursToCheck)
 
-  return {
-    stats,
-    spinsInTimeFrame,
-  };
-};
+    return {
+        stats,
+        spinsInTimeFrame,
+    }
+}
